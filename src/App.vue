486f25e53fc56d594d4f6b926c0769aef84be641
panhe-xue <1158817942@qq.com>
2019-04-12 00:32:24 +0800

    a

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,31 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <el-container>
+      <el-header style="padding: 0!important;">
+        <Header></Header>
+      </el-header>
+      <el-main><router-view/></el-main>
+      <el-footer style="border: 1px solid red">Footer</el-footer>
+    </el-container>
   </div>
 </template>
 
+<script>
+  import Header from '@/views/layout/Header'
+  export default {
+    name: 'app',
+    components: {
+      Header
+    }
+  }
+</script>
 <style>
+*{
+  margin: 0;
+  padding: 0;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/views/layout/Header.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="header">
+        <el-row>
+            <el-col :span="4">
+                <div>{{title}}</div>
+            </el-col>
+            <el-col :span="20">
+                <div class="menu" style="justify-content:flex-end;">
+                    <el-menu
+                    default-active="1"
+                    class="el-menu-demo"
+                    mode="horizontal"
+                    @select="handleSelect"
+                    :background-color="backgroundColor"
+                    text-color="#fff"
+                    active-text-color="">
+                        <el-menu-item v-for="(item, index) in menu" :index="index+1" :key="index" :data-color="item.backgroundColor">
+                            {{item.text}}
+                        </el-menu-item>
+                    </el-menu>
+                </div>
+            </el-col>
+        </el-row>
+    </div>
+</template>
+<script>
+export default {
+    name: 'headers',
+    data() {
+        return {
+            title: '吃亏才是福',
+            backgroundColor: 'blue',
+            menu: [
+                {
+                    text: ' HOME',
+                    backgroundColor: 'blue',
+                    note: '吃亏才是福'
+                },
+                {
+                    text: ' ABOUT',
+                    backgroundColor: 'green',
+                    note: '学苟知本，六经皆我注脚'
+                },
+                {
+                    text: ' ARCHIVE',
+                    backgroundColor: 'orange',
+                    note: '看到的，是我笨方法练习写作的所有文章'
+                },
+                {
+                    text: ' TAGS',
+                    backgroundColor: 'red',
+                    note: '哈哈，你找到了我的文章基因库'
+                },
+                {
+                    text: ' WORKS',
+                    backgroundColor: 'semantic',
+                    note: '知之真切笃实处即是行，行之明觉精察处即是知'
+                }
+            ]
+        }
+    },
+    methods: {
+        handleSelect(key, keyPath) {
+            console.log(key, keyPath)
+        }
+    }
+}
+</script>
+<style scoped>
+    
+</style>
