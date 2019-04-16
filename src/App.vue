486f14dfeb520d9d7ff8658afcc92dbcfdf59ee5
panhe-xue <1158817942@qq.com>
2019-04-16 23:39:25 +0800

    a

--- src/App.vue
+++ src/App.vue
@@ -32,6 +32,7 @@ export default {
 *{
   margin: 0;
   padding: 0;
+  line-height: 1.7;
 }
 #app {
   font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,17 +1,76 @@
 <template>
-  <div id="index">
-      首页
+  <div class="home">
+    <div class="homeContainer">
+      <div class="row" v-for="(item, index) in test" :key="index" >
+        <router-link class="router-link" to="/article/1">
+          <h2 class="h2">标题</h2>
+          <span class="text">下载地址：https://github.com/cnfeat/GoodThingList/blob/master/Book_LearnWritingTheHardWay.pdf 百度云下载地址：链接: https://pan.baidu.com/s/12buyBW9P2B...</span>
+          <p class="date">Posted on 2019-12-31</p>
+        </router-link>
+      </div>
+      <el-pagination
+      style="margin: 30px auto;"
+      layout="prev, pager, next"
+      :total="50">
+    </el-pagination>
+    </div>
   </div>
 </template>
 
+<style>
+.home{
+  width: 100%;
+  height: auto;
+  box-sizing: border-box;
+  margin: 30px 0;
+}
+.homeContainer{
+  width: 1170px;
+  margin: 0 auto;
+}
+.row{
+  box-sizing: border-box;
+  padding: 30px 15px;
+  text-align: left;
+  width: 75%;
+  margin: 0 auto;
+  border-bottom: 1px solid #ccc;
+  margin: 20px auto;
+}
+.router-link {
+  text-decoration: none;
+  color: #a3a3a3;
+  font-size: 14px;
+}
+.h2{
+  font-size: 25px;
+  font-weight: 800;
+  line-height: 2.5;
+  color: #000;
+}
+.text{
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+.h2:hover,.text:hover,.date:hover{
+  color: #404040;
+}
+.date{
+  font-family: 'Lora', 'Times New Roman', serif;
+  color: #808080;
+  font-size: 12px;
+  font-style: italic;
+}
+</style>
 <script>
-
 export default {
   name: 'home',
+  data () {
+    return {
+      test: [1, 2, 3, 4, 5]
+    }
+  },
   components: {
   }
 }
 </script>
-<style>
-
-</style>
